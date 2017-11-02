4920c1b71a92c14bf89fe276d37d689af845992c
KocproZ <kocproz@protonmail.com>
2017-11-02 20:24:49 +0100

    First

---
+++ src/pl/kocproz/fx/Main.java
@@ -0,0 +1,93 @@
+package pl.kocproz.fx;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        Random r = new Random();
+//        for (int i = 0; i < 30; i++) {
+//            inputList[i] = r.nextInt(999);
+//            System.out.print(inputList[i] + ",");
+//        }
+//        System.out.println();
+//
+//
+//        for (int i : outputList) {
+//            System.out.print(i + ",");
+//        }
+
+    }
+
+    public static int[] sort(int[] inputList) {
+        int[] numbersList = new int[10];
+        int[] outputList = new int[inputList.length];
+        int digitBuffer;
+
+        for (int j = 1; j < new String("" + Integer.MAX_VALUE).length(); j++) {
+            for (int i = 0; i < inputList.length; i++) {
+                numbersList[getDigit(inputList[i], j)]++;
+            }
+
+            for (int i = 1; i < 10; i++) {
+                numbersList[i] += numbersList[i - 1];
+            }
+
+            for (int i = inputList.length - 1; i > -1; i--) {
+                digitBuffer = getDigit(inputList[i], j);
+                numbersList[digitBuffer]--;
+                outputList[numbersList[digitBuffer]] = inputList[i];
+            }
+            System.arraycopy(outputList, 0, inputList, 0, inputList.length);
+            Arrays.fill(numbersList, 0);
+        }
+        return outputList;
+    }
+
+    public static int[] sortBinary(int[] inputList) {
+        int[] numbersList = new int[2];
+        int[] outputList = new int[inputList.length];
+        int digitBuffer;
+
+        for (int j = 0; j < 32; j++) {
+            for (int i = 0; i < inputList.length; i++) {
+                numbersList[getBinaryDigit(inputList[i], j)]++;
+            }
+
+            numbersList[1] += numbersList[0];
+
+            for (int i = inputList.length - 1; i > -1; i--) {
+                digitBuffer = getBinaryDigit(inputList[i], j);
+                numbersList[digitBuffer]--;
+                outputList[numbersList[digitBuffer]] = inputList[i];
+            }
+            System.arraycopy(outputList, 0, inputList, 0, inputList.length);
+            Arrays.fill(numbersList, 0);
+        }
+        return outputList;
+    }
+
+    public static int getBinaryDigit(int number, int digit) {
+        return (number >> digit) & 1;
+    }
+
+    public static int getFirstDigit(int number) {
+        return number % 10;
+    }
+
+    public static int getSecondDigit(int number) {
+        return ((number / 10)) % 10;
+    }
+
+    public static int getThirdDigit(int number) {
+        return ((number / 100)) % 10;
+    }
+
+    public static int getDigit(int number, int digit) {
+        int divider = (int) Math.pow(10, digit - 1);
+        return (number / divider) % 10;
+    }
+
+}
---
+++ tests/MainTest.java
@@ -0,0 +1,74 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+import pl.kocproz.fx.Main;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by kacper on 6/17/17.
+ */
+public class MainTest {
+
+    static int cases = 9000000;
+    static int[] inputList = new int[cases];
+    static int[] expected;
+
+    @BeforeClass
+    public static void beforeClass() {
+        Random r = new Random();
+        for (int i = 0; i < cases; i++) {
+            inputList[i] = r.nextInt(9000000);
+        }
+        expected = Arrays.copyOf(inputList, cases);
+    }
+
+    @Test
+    public void sortTest() {
+        int[] outputList = Main.sort(Arrays.copyOf(inputList, cases));
+        Arrays.sort(expected);
+        assertArrayEquals(expected, outputList);
+    }
+
+    @Test
+    public void sortBinaryTest() {
+        int[] outputList = Main.sortBinary(Arrays.copyOf(inputList, cases));
+        Arrays.sort(expected);
+        assertArrayEquals(expected, outputList);
+    }
+
+    @Test
+    public void getFirstDigitTest() {
+        int a = 88;
+        int result = Main.getFirstDigit(a);
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void getSecondDigitTest() {
+        int a = 79;
+        int result = Main.getSecondDigit(a);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void getThirdDigitTest() {
+        int a = 32;
+        int result = Main.getThirdDigit(a);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void getDigitTest() {
+        int a = 421907;
+        assertEquals(7, Main.getDigit(a, 1));
+        assertEquals(0, Main.getDigit(a, 2));
+        assertEquals(9, Main.getDigit(a, 3));
+        assertEquals(1, Main.getDigit(a, 4));
+        assertEquals(2, Main.getDigit(a, 5));
+    }
+
+}
